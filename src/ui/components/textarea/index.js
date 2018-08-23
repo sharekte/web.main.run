2edd3eea13bfac794f277a4a3ac467beddc7125b
dangcheng <dangcheng@hotmail.com>
2018-08-23 23:05:24 +0800

    ui textarea

---
+++ src/ui/components/textarea/index.js
@@ -0,0 +1,7 @@
+import MTextarea from "./src/Textarea"
+
+MTextarea.install = function(Vue) {
+    Vue.component(MTextarea.name, MTextarea);
+};
+
+export default MTextarea;
---
+++ src/ui/components/textarea/src/Textarea.vue
@@ -0,0 +1,60 @@
+<template>
+    <textarea
+        class="m-textarea"
+        ref="textarea"
+        v-bind="$attrs"
+        :value="currentValue"
+        @input="onInput"
+        @focus="onFocus"
+        @blur="onBlur"
+        :class="[
+            disabled ? 'disabled' : '',
+            size ? size: '',
+        ]"
+    ></textarea>
+</template>
+<script>
+export default {
+  name: "MInput",
+  data() {
+    return {
+      currentValue: this.value
+    };
+  },
+  props: {
+    value: {
+      type: [String, Number],
+      default: ""
+    },
+    size: String,
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    inputType() {
+      return this.type;
+    }
+  },
+  methods: {
+    onInput(event) {
+      const value = event.target.value;
+
+      this.$emit("input", value);
+      this.currentValue = value;
+    },
+    onFocus() {
+      // console.log("focus");
+    },
+    onBlur() {
+      // console.log("blur");
+    }
+  },
+  watch: {
+    value(val, oldValue) {
+      this.currentValue = val;
+    }
+  }
+};
+</script>
